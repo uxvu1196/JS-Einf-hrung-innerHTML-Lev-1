65338370bebbc30466684d45f27173cb4f43c986
uxvu1196 <uxvu1196@gmail.com>
2021-07-19 17:46:10 +0200

    first commit

---
+++ assets/js/Lev1_2.js
@@ -0,0 +1,10 @@
+document.getElementById("info").innerHTML="<h1>Hello World</h1>"
+document.getElementById("info").innerHTML +="<h2> How are you?</h2>"
+
+
+// += sind Zuweisungsoperatoren = Sinn: Damit der Text darauffolgend angehängt wird - der Plus macht bei STring Sinn
+
+
+document.getElementById('container').innerHTML="<p>start of the element</p>"
+document.write("end of the element")
+
---
+++ assets/js/Lev1_3.js
@@ -0,0 +1,5 @@
+document.getElementById("gallery").innerHTML='<figure style="display:inline-block"><img src="https://unsplash.it/300" alt="test"><figcaption>Fig.1</figcaption></figure> '
+document.getElementById("gallery").innerHTML+='<figure style="display:inline-block"><img src="https://unsplash.it/300" alt="test"><figcaption>Fig.2</figcaption></figure>'
+document.getElementById("gallery").innerHTML+='<figure style="display:inline-block"><img src="https://unsplash.it/300" alt="test"><figcaption>Fig.3</figcaption></figure>'
+
+// figure style="display:inline-block"
